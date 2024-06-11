30e470e40697f7185ded31948e571a5ad4ee9094
aligbillow <alibillow0@gmail.com>
2024-06-11 16:08:03 -0500

    lil edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Header } from './components/Header';
-// import { TodoItem } from './components/TodoItem/TodoItem';
 import { TodoList } from './components/TodoList/TodoList';
 
 interface Task {
